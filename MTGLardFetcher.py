918eecbbc8995de3f6995f9f1dd8f8f68708efba
wiewarm.ch <info@wiewarm.ch>
2017-11-04 10:56:45 +0100

    Update PRAW and respect Off-Topic Thread

--- MTGLardFetcher.py
+++ MTGLardFetcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import praw
 import re
@@ -9,6 +9,7 @@ import sqlite3
 import os
 from tendo import singleton
 import sys
+import pprint
 
 
 reload(sys)
@@ -18,20 +19,30 @@ conn = sqlite3.connect('state.sqlite')
 cursor = conn.cursor()
 persistence = True
 
-def check_condition(c):
+
+def check_condition(c, r):
+    pp = pprint.PrettyPrinter(indent=4)
     text = c.body
     matches = get_matches(text)
     comment_id =  c.id
     comment_author = str(c.author)
     cursor.execute('SELECT comment_id FROM replied WHERE comment_id=?', (c.id, ))
     already_replied = cursor.fetchone()
+    holytopic = 'Weekly Off-Topic Thread' 
 
-    print "checking comment " + comment_id + " matches " + str(matches) + " replied " + str(already_replied)
+    #print "checking comment in thread " + pp.pformat(vars(c)) + " -- " + comment_id + " matches " + str(matches) + " replied " + str(already_replied)
+    #print "checking comment in thread -" + c.link_title + "-:" + holytopic +  ": -- " + comment_id + " matches " + str(matches) + " replied " + str(already_replied)
 
     # don't reply twice and never reply to own comment to prevent possible loops
-    if matches and already_replied is None and c.author is not 'MTGLardFetcher':
+    if (matches and 
+        already_replied is None and 
+        c.author != 'MTGLardFetcher' and 
+        c.link_title != holytopic):
+
+        print "replying to unanswered comment " + comment_id
         return matches
     else:
+        print "ignoring answered/uneligible comment " + comment_id
         return False
 
 def get_matches(text):
@@ -40,9 +51,10 @@ def get_matches(text):
     return matches
 
 def get_links(r):
-    subreddit = r.get_subreddit('MTGLardFetcher')
+    #subreddit = r.get_subreddit('MTGLardFetcher')
+    subreddit = r.subreddit('MTGLardFetcher')
     candidates = ['http://i.imgur.com/66Knlyo.png'] # pot of greed is always an option
-    for post in subreddit.get_hot(limit=50):
+    for post in subreddit.hot(limit=50):
         if not "/r/MTGLardFetcher" in post.url:
             # allow only serious domains 
             if re.search('(i.redd.it|i.imgur.com)', post.url):
@@ -81,8 +93,6 @@ def bot_action(c, matches, links, respond=False):
     if respond:
         try:
             c.reply(text)
-            #cursor.execute('insert into replied (comment_id) values (?)', (c.id, ))
-            #conn.commit()
         except praw.errors.APIException:
             print "TOO_OLD or some other crap, going on"
         finally: 
@@ -98,32 +108,29 @@ if __name__ == '__main__':
     conn.commit()
 
     UA = 'MTGLardFetcher, a MTGCardFetcher Parody bot for /r/magicthecirclejerking. Kindly direct complaints to /u/0x2a'
-    r = praw.Reddit(UA)
+    #r = praw.Reddit(UA)
 
-    r.set_oauth_app_info(client_id=os.getenv('CLIENT_ID'), 
-            client_secret=os.getenv('CLIENT_SECRET'), 
-            redirect_uri='http://127.0.0.1:666/authorize_callback') 
-    r.refresh_access_information(os.getenv('REFRESH_TOKEN'))
-
-    # get initial auth and refresh token, then paste the refresh token into refresh_access_information() 
-    #url = r.get_authorize_url('xxx1238', ['submit', 'read'] , True)
-    #print url
-    #access_information = r.get_access_information('access token from redirect above')
-    #print access_information
-    #add refresh token to ENV
+    r = praw.Reddit( user_agent=UA, 
+            client_id=os.getenv('CLIENT_ID'),
+            client_secret=os.getenv('CLIENT_SECRET'),
+            refresh_token=os.getenv('REFRESH_TOKEN')
+            )
+    
 
     last_refresh = int(time.time())
     links = get_links(r)
 
+    #target = '0x2a_personal' 
+    target = 'magicthecirclejerking' 
 
-    #for c in praw.helpers.comment_stream(r, 'magicthecirclejerking'):
-    for c in praw.helpers.comment_stream(r, 'magicthecirclejerking'):
-        matches = check_condition(c)
+    for c in r.subreddit(target).stream.comments():
+        matches = check_condition(c, r)
         if matches:    
             now = int(time.time()) 
             if (now - last_refresh > 60): 
                 links = get_links(r)
                 last_refresh = now
+
             bot_action(c, matches, links, True)
 
     conn.close()
